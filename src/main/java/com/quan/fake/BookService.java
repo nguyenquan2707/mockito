845f660195414ad7eb819178792043c8dd692542
Quan Nguyen <vozermuonnam1@gmail.com>
2023-10-28 14:34:59 +0700

    .

---
+++ src/main/java/com/quan/fake/BookService.java
@@ -0,0 +1,9 @@
+package com.quan.fake;
+
+import com.quan.fake.repo.BookRepository;
+
+public class BookService {
+
+    private BookRepository bookRepository;
+
+}
---
+++ src/main/java/com/quan/fake/repo/BookRepository.java
@@ -0,0 +1,4 @@
+package com.quan.fake.repo;
+
+public interface BookRepository {
+}
