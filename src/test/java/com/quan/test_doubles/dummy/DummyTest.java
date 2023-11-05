b49e9da0168c4068fe897a66c12b80cf14b98556
Quan Nguyen <vozermuonnam1@gmail.com>
2023-11-05 09:00:11 +0700

    mockito

--- src/test/java/com/quan/test_doubles/dummy/DummyTest.java
+++ src/test/java/com/quan/test_doubles/dummy/DummyTest.java
@@ -5,8 +5,11 @@ import com.quan.dummy.EmailService;
 import com.quan.dummy.Book;
 import com.quan.dummy.BookRepository;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collection;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -25,6 +28,25 @@ public class DummyTest {
 
         bookService.addBook(new Book("id1", "Java", 100D, LocalDate.now()));
         bookService.addBook(new Book("id2", "JavaScript", 90D, LocalDate.now()));
+
+        assertEquals(2, bookService.numberOfBook());
+
+    }
+
+    @Test
+    public void demoDummyWithMockito() {
+
+        BookRepository bookRepository = Mockito.mock(BookRepository.class);
+        EmailService emailService = Mockito.mock(EmailService.class);
+        BookService bookService = new BookService(bookRepository, emailService);
+        Book book = new Book("id1", "Java", 100D, LocalDate.now());
+        Book book2 = new Book("id2", "JavaScript", 90D, LocalDate.now());
+
+        Collection<Book> books = new ArrayList<>();
+        books.add(book);
+        books.add(book2);
+
+        Mockito.when(bookRepository.findAll()).thenReturn(books);
 
         assertEquals(2, bookService.numberOfBook());
 
--- src/test/java/com/quan/test_doubles/stub/StubTest.java
+++ src/test/java/com/quan/test_doubles/stub/StubTest.java
@@ -5,6 +5,7 @@ import com.quan.stub.BookRepository;
 import com.quan.stub.BookService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.util.List;
 
@@ -19,6 +20,14 @@ public class StubTest {
         List<Book> newBooksWithAppliedDiscount = bookService.getNewBooksWithAppliedDiscount(10, 7);
         Assertions.assertEquals(2, newBooksWithAppliedDiscount.size());
         Assertions.assertEquals(225, newBooksWithAppliedDiscount.get(0).getPrice());
+    }
+    @Test
+    public void demoStubWithMockito() {
+
+        BookRepository bookRepository = Mockito.mock(BookRepository.class);
+        BookService bookService = new BookService(bookRepository);
+
+
     }
 
 }
