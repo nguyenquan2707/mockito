fb02744b928e58948b18a1a1ff2074187d605e56
Quan Nguyen <vozermuonnam1@gmail.com>
2023-11-05 21:33:01 +0700

    stubbing

--- src/test/java/com/quan/test_doubles/annotation/stubbing/BookServerTest.java
+++ src/test/java/com/quan/test_doubles/annotation/stubbing/BookServerTest.java
@@ -1,12 +1,19 @@
 package com.quan.test_doubles.annotation.stubbing;
 
+import com.quan.annotation.stubbing.Book;
 import com.quan.annotation.stubbing.BookRepository;
 import com.quan.annotation.stubbing.BookService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
 
 @ExtendWith(value = MockitoExtension.class)
 public class BookServerTest {
@@ -20,5 +27,18 @@ public class BookServerTest {
     @Test
     public void testCalculateTotalCostOfBooks() {
 
+        List<String> bookIds = Arrays.asList("1111", "2222");
+
+        Book book = new Book("1234", "Mockito",250, LocalDate.now());
+        Book book2 = new Book("1235", "Mockito5",250, LocalDate.now());
+
+        Mockito.when(bookRepository.findByBookId("1111")).thenReturn(book);
+        Mockito.when(bookRepository.findByBookId("2222")).thenReturn(book2);
+        int totalCost = bookService.calculateTotalCost(bookIds);
+
+        Assertions.assertEquals(500, totalCost);
+
+        Mockito.verify(bookRepository, Mockito.times(1)).findByBookId("1111");
+        Mockito.verify(bookRepository, Mockito.times(1)).findByBookId("2222");
     }
 }
