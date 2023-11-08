7b7a09c27b6ff19d9f360865fae922ad2e8d24ea
Quan Nguyen <vozermuonnam1@gmail.com>
2023-11-08 22:02:55 +0700

    ArgumentCaptor using annotations

---
+++ src/test/java/com/quan/test_doubles/argument_captor/BookServiceTest2.java
@@ -0,0 +1,41 @@
+package com.quan.test_doubles.argument_captor;
+
+
+import com.quan.arguent_captor.Book;
+import com.quan.arguent_captor.BookRepository;
+import com.quan.arguent_captor.BookRequest;
+import com.quan.arguent_captor.BookService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+
+@ExtendWith(value = MockitoExtension.class)
+public class BookServiceTest2 {
+
+    @InjectMocks
+    private BookService bookService;
+
+    @Mock
+    private BookRepository bookRepository;
+
+    @Captor
+    private ArgumentCaptor<Book> bookArgumentCaptor;
+
+    @Test
+    public void testSaveBookWithBookRequest() {
+
+        BookRequest bookRequest = new BookRequest("Mockito", 600, LocalDate.now());
+
+        bookService.addBook(bookRequest);
+        Mockito.verify(bookRepository).save(bookArgumentCaptor.capture());
+
+        Book book = bookArgumentCaptor.getValue();
+        Assertions.assertEquals("Mockito", book.getTitle());
+        Assertions.assertEquals(600, book.getPrice());
+    }
+
+}
