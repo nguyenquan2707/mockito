a060f83724cf04e814e0e894fa8232bb6a17d03f
Quan Nguyen <vozermuonnam1@gmail.com>
2023-11-11 09:06:27 +0700

    BDD

--- src/test/java/com/quan/test_doubles/bdd/stubbing/StubTest.java
+++ src/test/java/com/quan/test_doubles/bdd/stubbing/StubTest.java
@@ -4,6 +4,7 @@ import com.quan.bdd.stubbing.Book;
 import com.quan.bdd.stubbing.BookRepository;
 import com.quan.bdd.stubbing.BookService;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -24,7 +25,7 @@ public class StubTest {
     private BookRepository bookRepository;
 
     @Test
-    public void demoStubWithMockito() {
+    public void testInTraditionalMockitoStyle() {
 
         Book book = new Book("1234", "Mockito",250, LocalDate.now());
         Book book2 = new Book("1235", "Mockito5",250, LocalDate.now());
@@ -37,6 +38,16 @@ public class StubTest {
 
         Assertions.assertEquals(2, bookService.getNewBooksWithAppliedDiscount(10, 7).size());
 
+    }
+
+    @Test
+    @DisplayName("given - New Books, When get New Books With Applied Discount Is Called, Then New Books With Discount Is Returned")
+    public void test_GivenNewBooks_When_getNewBooksWithAppliedDiscountIsCalled_ThenNewBooksWithDiscountIsReturned() {
+        //Arrange - Given
+
+        //act -when
+
+        //Assert - Then
     }
 
 }
